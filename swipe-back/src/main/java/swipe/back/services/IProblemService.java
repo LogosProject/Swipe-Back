9c37459927cd9af92f31c281e27bc403342815e6
Nicorr <reynolds.nicorr@gmail.com>
2015-01-15 15:28:41 +0100

    ProblemService ajouté

---
+++ swipe-back/src/main/java/swipe/back/services/IProblemService.java
@@ -0,0 +1,10 @@
+package swipe.back.services;
+
+import swipe.back.domain.Problem;
+import swipe.back.domain.SolutionScore;
+import swipe.back.domain.User;
+import swipe.back.domain.ValueSolutionScore;
+import swipe.back.domain.Versus;
+
+public interface IProblemService {
+}
---
+++ swipe-back/src/main/java/swipe/back/services/ProblemService.java
@@ -0,0 +1,16 @@
+package swipe.back.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import swipe.back.dao.SolutionRepository;
+import swipe.back.dao.UserRepository;
+import swipe.back.domain.Problem;
+import swipe.back.domain.Solution;
+import swipe.back.domain.SolutionScore;
+import swipe.back.domain.User;
+import swipe.back.domain.ValueSolutionScore;
+import swipe.back.domain.Versus;
+
+public class ProblemService implements IProblemService {
+
+}
